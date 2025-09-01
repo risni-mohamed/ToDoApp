fe554c731fe66d3473a5eb217ec381be9dce1a91
risni-mohamed <mohamadrizni42@gmail.com>
2025-09-01 21:51:36 +0530

    Prepare for Vercel deployment

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r4hkm2#(j4a-z4gf5*$f98e9vtl7d7=nc0hq(1fqz!r9*p87&y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
